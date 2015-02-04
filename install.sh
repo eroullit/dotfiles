b71ff7b6329e33ccfb4f173fcd3368ed0d031ef8
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2015-02-04 20:40:12 +0000

    awesome-fonts: install awesome fonts in /home.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ DOT="bashrc gitconfig vimrc.local mrconfig tmux.conf oh-my-git"
 
 ./spf-13-vim/bootstrap.sh
 
+mkdir -p ~/.fonts
+cp "$PWD"/awesome-terminal-fonts/patched/*.ttf ~/.fonts
+
 for f in $DOT
 do
     ln -sf "$PWD/$f" "$HOME/.$(basename $f)"
