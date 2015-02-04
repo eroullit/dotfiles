7610b0cd72e553e877935b5ea931b45386e91b1d
Emmanuel Roullit <emmanuel.roullit@gmail.com>
2015-02-04 12:37:21 +0000

    oh-my-git: install oh-my-git prompt script.
    Signed-off-by: Emmanuel Roullit <emmanuel.roullit@gmail.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-DOT="bashrc gitconfig vimrc.local mrconfig tmux.conf"
+DOT="bashrc gitconfig vimrc.local mrconfig tmux.conf oh-my-git"
 
 ./spf-13-vim/bootstrap.sh
 
 for f in $DOT
 do
-    ln -sf "$PWD/$f" "$HOME/.$f"
+    ln -sf "$PWD/$f" "$HOME/.$(basename $f)"
 done
 
 # vim: ft=sh:tabstop=4:et
